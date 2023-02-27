18df86d3ed27297985522372975da03f2c187c0f
楼律江 <luax@qq.com>
2023-02-27 13:38:10 +0800

    initial

--- param.go
+++ param.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func GetInt64Param(c gin.Context, key string) int64 {
+func GetInt64Param(c *gin.Context, key string) int64 {
 
 	str := c.Param(key)
 
@@ -15,7 +15,7 @@ func GetInt64Param(c gin.Context, key string) int64 {
 
 }
 
-func GetIdParam(c gin.Context) int64 {
+func GetIdParam(c *gin.Context) int64 {
 
 	return GetInt64Param(c, "id")
 
